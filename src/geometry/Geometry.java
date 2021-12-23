759dd066b47d03190836030c84de9463657ca12b
Avinash <Avichauhan111196@gmail.com>
2021-12-23 14:11:18 +0530

    Added : Different length comparison added

--- src/geometry/Geometry.java
+++ src/geometry/Geometry.java
@@ -18,19 +18,23 @@ public class Geometry {
 
         length1 = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
 
-        System.out.println("The length is " + length1);
+        System.out.println("The length of line1 " + length1);
 
         length2 = Math.sqrt(Math.pow(x4-x3,2) + Math.pow(y4-y3,2));
 
+        System.out.println("The length of line2 " + length2);
+
         if (length1 == length2)
         {
-
-            System.out.println("Length is equal");
+            System.out.println("Length of both line is equal");
         }
-        else
+        else if (length1 < length2)
         {
-            System.out.println("Length is not equal");
-
+            System.out.println("Length of line1 is less than line2");
+        }
+        else if (length1 > length2)
+        {
+            System.out.println("Length of line1 is greater than length2");
         }
     }
 }
